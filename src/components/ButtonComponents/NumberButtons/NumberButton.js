124c64a04fbfac53c293bdd6db348efaafcf149d
Jeffreyo3 <Jeffrey.Orndorff@gmail.com>
2019-09-25 18:06:31 -0700

    Updated button formatting to stretch Zero key

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -4,7 +4,7 @@ const NumberButton = (props) => {
 
   return (
 
-    <button className={props.numberProp === "0" ? "numbersButton zeroButton" : "numberButton"} onClick={() => props.clickNumberProp(props.numberProp)}>
+    <button className={props.numberProp === "0" ? "numberButton zeroButton" : "numberButton"} onClick={() => props.clickNumberProp(props.numberProp)}>
       {props.numberProp}
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </button>
